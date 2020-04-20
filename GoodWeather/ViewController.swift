0d323dacd087c711475e63fafe305fdc11f20686
Serhan Khan <serhan@cocooncreations.net>
2020-04-20 18:46:30 +0300

    Initial Commit

---
+++ GoodWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoodWeather
+//
+//  Created by Serhan Khan on 20/04/2020.
+//  Copyright © 2020 Serhan Khan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
